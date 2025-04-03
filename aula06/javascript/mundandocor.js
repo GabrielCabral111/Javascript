2adf1b9137eb6a4b5581a6a9144132cbcaf564cb
Gabriel Cabral De Paula <gabrielcabral072@gmail.com>
2025-04-02 23:44:03 -0300

    novas atividades
    atividades de javascript

---
+++ aula06/javascript/mundandocor.js
@@ -0,0 +1,59 @@
+
+
+
+
+function preto(){
+    document.querySelector("#fundo").style.backgroundColor="black";document.querySelector("#fundo").style.color="white";
+    document.querySelector(".img").src="imagem/fundo3 (1).jpg";
+    texto.innerHTML =" Teste de hdd"
+    let texto = document.getElementById("titulo");
+
+    
+}
+
+
+function dau(){
+    document.querySelector("#fundo").style.backgroundColor="orchid";document.querySelector("#fundo").style.color="lawngreen";
+    document.querySelector(".img").src="imagem/daltonismo.jpg";
+    texto.innerHTML =" Teste de hdd"
+    let texto = document.getElementById("titulo");
+
+    
+}
+function vermelho(){
+    document.querySelector("#fundo").style.backgroundColor="brown";document.querySelector("#fundo").style.color="white";
+    document.querySelector(".img").src="imagem/fundo3 (1).jpg";
+    texto.innerHTML =" Teste de hdd"
+    let texto = document.getElementById("titulo");
+
+    
+}
+
+function home(){
+    document.querySelector("#fundo").style.backgroundColor="antiquewhite";document.querySelector("#fundo").style.color="brown";
+    document.querySelector(".img").src="imagem/fundo (1).jpg";
+    document.querySelector("#fundo").style.fontSize="16px";
+}
+
+function azul(){
+    document.querySelector("#fundo").style.backgroundColor="cornflowerblue";document.querySelector("#fundo").style.color="white";
+    document.querySelector(".img").src="imagem/imagem4 (1).jpg";
+}
+
+function mais(){
+    document.querySelector("#fundo").style.backgroundColor="cornflowerblue";document.querySelector("#fundo").style.color="white";
+    document.querySelector(".img").src="imagem/imagem4 (1).jpg";
+    document.querySelector("#fundo").style.fontSize="30px";
+}
+
+
+
+
+
+
+
+
+
+
+
+titulo
